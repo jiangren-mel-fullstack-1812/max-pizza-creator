1db524b4f9f1b380fb3a874fa69c5c1875256964
yuhe3000 <yanliqiang66@gmail.com>
2019-03-02 11:49:09 +1100

    create pizza-creator api

---
+++ README.md
@@ -0,0 +1,3 @@
+# pizza-creator-system
+
+[![LoopBack](https://github.com/strongloop/loopback-next/raw/master/docs/site/imgs/branding/Powered-by-LoopBack-Badge-(blue)-@2x.png)](http://loopback.io/)
---
+++ src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,24 @@
+import {PizzaCreatorSystemApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const app = new PizzaCreatorSystemApplication({
+    rest: givenHttpServerConfig(),
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: PizzaCreatorSystemApplication;
+  client: Client;
+}
